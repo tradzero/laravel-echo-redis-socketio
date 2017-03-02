77d308e3d71807e192d730e9041fecd7f432ab08
林振宇 <admin@drakframe.com>
2017-03-02 16:44:28 +0800

    update SendMsgEvent.php for broadcast

--- app/Events/SendMsgEvent.php
+++ app/Events/SendMsgEvent.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class SendMsgEvent
+class SendMsgEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
